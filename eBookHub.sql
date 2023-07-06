36291214e6baf56590b0ec8a8acd914d6276f5dd
Ottoniel Menéndez <zmenendez-2019484@kinal.edu.gt>
2023-07-06 15:09:41 -0600

    Se crea la entidad Libro

--- eBookHub.sql
+++ eBookHub.sql
@@ -34,3 +34,49 @@ Create table Categoria (
     descripcionCategoria varchar(250) not null,
 	primary key PK_idCategoria (idCategoria)
 );
+
+Create table Libro(
+	idLibro int not null,
+    tituloLibro varchar(100) not null,
+    descripcionLibro varchar(250) not null,
+    fechaDePublicacion date not null,
+    idiomaLibro varchar(30) not null,
+    costoLibro decimal(10,2) not null,
+    idTipoLibro int not null,
+    idAutor int not null,
+    idEditorial int not null,
+    idCategoria int not null,
+    primary key PK_idLibro(idLibro),
+    constraint FK_Libro_TipoLibro foreign key (idTipoLibro)
+		references TipoLibro(idTipoLibro),
+    constraint FK_Libro_Autor foreign key (idAutor)
+		references Autor(idAutor),
+    constraint FK_Libro_Editorial foreign key (idEditorial)
+		references Editorial(idEditorial),
+    constraint FK_Libro_Categoria foreign key (idCategoria)
+		references Categoria(idCategoria)
+);
+/*
+-- Procedimientos Almacenados de la Entidad Libros --
+Delimiter $$
+	create procedure sp_AgregarLibro(in idLibro int, in tituloLibro varchar(100), in descripcionLibro varchar(250), in fechaDePublicacion date,
+		in idiomaLibro varchar(30), in costoLibro decimal(10,2), in idTipoLibro int, in idAutor int, in idEditorial int, in idCategoria  int)
+			Begin
+				INSERT INTO Libro (idLibro, tituloLibro, descripcionLibro, fechaDePublicacion, idiomaLibro, costoLibro, idTipoLibro, idAutor, 
+					idEditorial, idCategoria)
+						VALUES (idLibro, tituloLibro, descripcionLibro, fechaDePublicacion, idiomaLibro, costoLibro, idTipoLibro, idAutor,
+							idEditorial, idCategoria);
+            End $$
+Delimiter ;
+
+Delimiter $$
+	create procedure sp_ListarLibros()
+		Begin
+			Select * from Libro;
+        End$$
+Delimiter ;
+	create procedure sp_BuscarLibro(in id int)
+		Begin
+			Select * from Libro where idLibro = id
+        End$$
+Delimiter ;*/
